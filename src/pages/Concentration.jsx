5898321404dbbe9b44566b0d1cedf5e1d70c7cbe
TheDoug <dkarda@gmail.com>
2026-08-27 13:26:29 -0400

    change default concentration game

--- src/pages/Concentration.jsx
+++ src/pages/Concentration.jsx
@@ -36,7 +36,7 @@ const Concentration = () => {
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
   const [matches, setMatches] = useState(0);
-  const [theme, setTheme] = useState("rockalbums");
+  const [theme, setTheme] = useState("hockey");
   let themedCardImages = [];
 
   //shuffle cards
